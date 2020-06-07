0cea49d90ddd401154d0239439efcd8762b16fca
Paul-Arthur Thiéry <paul-arthur.thiery@dynamicly.com>
2020-06-07 04:36:39 -0400

    ⚡sort cities when parsing + jsdoc

--- src/data/cities-parser.js
+++ src/data/cities-parser.js
@@ -1,5 +1,24 @@
 const fs = require('fs').promises;
 const resolvePath = require('path').resolve;
+
+/** @module citiesParser */
+
+
+/**
+ *
+ * @description sorts a given list of cities by their name
+ * @param {Object[]} cities - list of cities
+ * @param {string} cities[].name
+ * @returns {Object[]} list of cities sorted alphabetically
+ */
+const sortCities = (cities) => cities.sort((a, b) => (a.name > b.name ? 1 : -1));
+
+/**
+ * @description parses a file containing city data into a list of cities
+ * @async
+ * @param {string} path - path to file containing list of all cities
+ * @return {Object[]} list of cities containing their name, latitude, longitude, and fullName
+ */
 const getCities = (path) => fs.readFile(resolvePath(__dirname, path), 'utf8')
   .then((data) => {
     const rows = data.split('\n');
@@ -15,6 +34,7 @@ const getCities = (path) => fs.readFile(resolvePath(__dirname, path), 'utf8')
         fullName: `${columns[1]}, ${columns[10]}, ${columns[8]}`,
       };
     });
+    return sortCities(cities);
   })
   .catch((err) => {
     throw err;
