23eb754a7ef1038b5a053285f423763bdb476add
Paul-Arthur Thiéry <paul-arthur.thiery@dynamicly.com>
2020-06-07 04:42:25 -0400

    ✨express config and route definition

---
+++ src/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const createError = require('http-errors');
+const resolvePath = require('path').resolve;
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const suggestionsRouter = require('./routes/index');
+
+const app = express();
+
+app.use(cors({ origin: process.env.FRONT_END_URL }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/suggestions', suggestionsRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+app.use(bodyParser.json());
+
+module.exports = app;
